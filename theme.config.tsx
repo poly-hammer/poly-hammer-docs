3b9ff0c69b502974b4a65838acbc59e6164b7e0b
Jack Yao <jack.yao@polyhammer.com>
2023-11-21 17:42:18 -0600

    changed logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Image
+  width={100}
+  height={40}
+  src="/logo.svg"
+  alt="Poly Hammer"
+  className="w-48 h-48"
+/>,
+  logoLink: 'https://www.polyhammer.com/logo.svg',
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // video: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
